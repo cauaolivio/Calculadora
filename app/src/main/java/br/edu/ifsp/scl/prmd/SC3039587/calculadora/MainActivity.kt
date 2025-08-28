5378c57777b26d0a93e82daedefce186a7dcd18d
Caua Olivio <caua.olivio@gmail.com>
2025-08-28 20:18:13 -0300

    feat: implementando funcionamento dos botoes numericos

--- app/src/main/java/br/edu/ifsp/scl/prmd/SC3039587/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prmd/SC3039587/calculadora/MainActivity.kt
@@ -1,20 +1,63 @@
 package br.edu.ifsp.scl.prmd.SC3039587.calculadora
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.prmd.SC3039587.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        val calculo = amb.calculo
+
+        amb.btn0.setOnClickListener {
+            calculo.text = "${calculo.text}0"
         }
+
+        amb.btn1.setOnClickListener {
+            calculo.text = "${calculo.text}1"
+        }
+
+        amb.btn2.setOnClickListener {
+            calculo.text = "${calculo.text}2"
+        }
+
+        amb.btn3.setOnClickListener {
+            calculo.text = "${calculo.text}3"
+        }
+
+        amb.btn4.setOnClickListener {
+            calculo.text = "${calculo.text}4"
+        }
+
+        amb.btn5.setOnClickListener {
+            calculo.text = "${calculo.text}5"
+        }
+
+        amb.btn6.setOnClickListener {
+            calculo.text = "${calculo.text}6"
+        }
+
+        amb.btn7.setOnClickListener {
+            calculo.text = "${calculo.text}7"
+        }
+
+        amb.btn8.setOnClickListener {
+            calculo.text = "${calculo.text}8"
+        }
+
+        amb.btn9.setOnClickListener {
+            calculo.text = "${calculo.text}9"
+        }
+
     }
 }
